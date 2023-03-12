69173fd1e985bbea10eff83d75f2bbf0f977184d
Ilya K <ilyusha_k@icloud.com>
2023-03-12 07:01:51 +0300

    Initial Commit

---
+++ raytracing/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  raytracing
+//
+//  Created by Ilya Kulinkovich on 3/12/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
